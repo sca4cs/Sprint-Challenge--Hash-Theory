e2ac3950925b2dfd87930d6bb98d561d9c48a940
sca4cs <shannon.atkinson.ca@gmail.com>
2018-11-16 15:03:17 -0500

    ex1

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -7,7 +7,28 @@ Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
   HashTable *ht = create_hash_table(16);
 
-  // YOUR CODE HERE
+  for (int i = 0; i < length; i++) {
+    hash_table_insert(ht, weights[i], i);
+  }
+
+  Answer *answer = malloc(sizeof(Answer));
+
+  for (int i = 0; i < length; i++) {
+    int w1 = limit - weights[i];
+    if (hash_table_retrieve(ht, w1) != -1) {
+      int i1 = i;
+      int i2 = hash_table_retrieve(ht, w1);
+      if (i1 > i2) {
+        answer->index_1 = i1;
+        answer->index_2 = i2;
+        return answer;
+      } else {
+        answer->index_1 = i2;
+        answer->index_2 = i1;
+        return answer;
+      }
+    }
+  }
 
   return NULL;
 }
